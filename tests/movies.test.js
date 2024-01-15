e142392ae8a52e703852b6e84cdb9efcdd3c4fbf
Pablo Martín Calvo <pablomc87@gmail.com>
2024-01-15 13:07:03 +0100

    added some tests

---
+++ tests/movies.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/api/movies");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET /api/movies/:id", () => {
+  it("should return a movie title", async () => {
+    const response = await request(app).get("/api/movies/1");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+
+    const response0 = await request(app).get("/api/movies/0");
+
+    expect(response0.status).toEqual(404);
+  });
+});
